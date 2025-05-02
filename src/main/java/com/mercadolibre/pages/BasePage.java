b02e013fd58c5f87f2a6b10101a1fa12336a36e8
storm7 <betopermar@gmail.com>
2025-05-02 16:39:54 -0600

    The script is almost complete and working but the price is giving troubles due to not always being found using the XPath. It will be commented so only the title of the product is shown.

--- src/main/java/com/mercadolibre/pages/BasePage.java
+++ src/main/java/com/mercadolibre/pages/BasePage.java
@@ -1,8 +1,14 @@
 package com.mercadolibre.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public class BasePage {
 
@@ -23,6 +29,22 @@ public class BasePage {
 
     protected void click(By locator){
         find(locator).click();
+
+    }
+
+    protected void scrollToElementJs(By locator){ //Possible usable method
+        WebElement element = find(locator);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    protected List<WebElement> setElementsList(By locator){
+        return driver.findElements(locator);
+    }
+
+    protected void waitUntilDisplayed(By locator){
+        Wait<WebDriver> wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(d -> find(locator).isDisplayed());
+
     }
 
 }
--- src/main/java/com/mercadolibre/pages/MexicoListadoPage.java
+++ src/main/java/com/mercadolibre/pages/MexicoListadoPage.java
@@ -1,67 +1,59 @@
 package com.mercadolibre.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 public class MexicoListadoPage extends BasePage {
 
     private By conditionFilterNuevo = By.xpath("/html/body/main/div/div[2]/aside/section[2]/div[5]/ul/li[1]/a");
     private By locationFilterDistritoFederal = By.xpath("//*[@id=\"root-app\"]/div/div[2]/aside/section[2]/div[10]/ul/li[1]/a");
-    private By dropdownOrderBy = By.id(":R1b56ie:-display-values");
+    //private By dropdownOrderBy = By.xpath("//button[@aria-labelledby = \":R1b56ie:-label :R1b56ie:-display-values\"]//div[@class=\"andes-dropdown__standalone-arrow\"]");
+    private By dropdownOrderBy = By.id(":R1b56ie:-trigger");
     private By orderByMayor = By.id(":R1b56ie:-menu-list-option-price_desc");
     private By cookieBanner = By.xpath("/html/body/div[4]/div[1]/div/div[2]/button[1]");
-    private List<By> firstFiveItems = new ArrayList<>();
-    private HashMap<String,String> itemsMap = new HashMap<String,String>();
+    private By itemsList = By.xpath("//div[@class=\"poly-card__content\"]");
 
-    public void clickCookiesBanner() throws  InterruptedException{
-        Thread.sleep(500);
+    public void clickCookiesBanner(){
+        waitUntilDisplayed(cookieBanner);
         click(cookieBanner);
     }
 
-    public void clickConditionFilter() throws InterruptedException {
-        Thread.sleep(500);
+    public void clickConditionFilter() {
+        waitUntilDisplayed(conditionFilterNuevo);
         click(conditionFilterNuevo);
     }
 
-    public void clicklocationFilterDistritoFederal() throws InterruptedException{
-        Thread.sleep(500);
+    public void clickLocationFilterDistritoFederal(){
+        waitUntilDisplayed(locationFilterDistritoFederal);
         click(locationFilterDistritoFederal);
+    }
+
+    public void clickOrderByMayor() throws InterruptedException {
+        Thread.sleep(500);
+        click(dropdownOrderBy);
+        //clickJs(dropdownOrderBy);
+        Thread.sleep(500);
+        click(orderByMayor);
     }
 
     public void getFirstFiveItems() throws InterruptedException {
         clickConditionFilter();
         clickCookiesBanner();
-        clicklocationFilterDistritoFederal();
-        clickorderByMayor();
-        itemsMap.put(find(By.xpath("//*[@id=\"root-app\"]/div/div[2]/section/div[6]/ol/li[1]/div/div/div/div[2]/h3/a/text()")).getText(),find(By.xpath("//*[@id=\"root-app\"]/div/div[2]/section/div[6]/ol/li[1]/div/div/div/div[2]/div/div[1]/div[1]/div/span/span[2]")).getText());
-        System.out.println(itemsMap);
-        firstFiveItems.add(By.xpath(""));
-        firstFiveItems.add(By.xpath(""));
-        firstFiveItems.add(By.xpath(""));
-        firstFiveItems.add(By.xpath(""));
-    }
+        clickLocationFilterDistritoFederal();
+        clickOrderByMayor();
+        waitUntilDisplayed(itemsList);
+        List<WebElement> list = setElementsList(itemsList);
 
-    public void clickorderByMayor() throws InterruptedException{
-        Thread.sleep(500);
-        click(dropdownOrderBy);
-        Thread.sleep(500);
-        click(orderByMayor);
-    }
 
-    public void startFilteringAndSorting() throws InterruptedException {
-        clickConditionFilter();
-        clickCookiesBanner();
-        clicklocationFilterDistritoFederal();
-        clickorderByMayor();
+        for(int i = 0; i < 5; i++){
+            String productString = list.get(i).findElement(By.xpath(".//a[@class=\"poly-component__title\"]")).getText();
+            //If the price is commented all names are caught correctly
+            //String priceString = list.get(i).findElement(By.xpath(".//div[@class=\"poly-price__current\"]/span[@class=\"andes-money-amount andes-money-amount--cents-superscript\"]/span[@class=\"andes-money-amount__fraction\"]")).getText();
+            //System.out.println("Product: " + productString + ". Price: " + priceString +".");
+            //Not sure why the code sometimes pushes 2 or 4 correct and then it suddenly fails. I think it has to do with the reviews, that's the only main difference between the last working xpath and the failing one
+            System.out.println("Product: " + productString + ".");
+        }
     }
-
 }
